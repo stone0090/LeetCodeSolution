d848b451d4ea8cd6821b64041df8ed46228c0fb5
stone <jiajie.sjj@alibabapictures.com>
2020-01-15 12:13:56 +0800

    commit 189

--- src/_189_Solution.java
+++ src/_189_Solution.java
@@ -36,37 +36,51 @@ public class _189_Solution {
 
     // 解法四
     // 执行耗时:1 ms,击败了81.21% 的Java用户
-    // 内存消耗:36.6 MB,击败了95.45% 的Java用户
+    // 内存消耗:38.2 MB,击败了74.66% 的Java用户
     public static void rotate3(int[] nums, int k) {
-        k = k % nums.length;
         int count = 0;
-        for (int start = 0; count < nums.length; start++) {
-            int current = start;
-            int previous = nums[start];
+        for (int i = 0; count < nums.length; i++) {
+            int previous_index = i;
+            int previous_value = nums[i];
             do {
-                int next = (count + k) % nums.length;
-                int temp = nums[next];
-                nums[next] = previous;
-                previous = temp;
-                current = next;
+                int next_index = (k + previous_index) % nums.length;
+                int temp = nums[next_index];
+                nums[next_index] = previous_value;
+                previous_value = temp;
+                previous_index = next_index;
                 count++;
-            } while (start != current);
+            } while (i != previous_index);
         }
     }
 
-    // 解法五
+    // 解法四
     // 执行耗时:0 ms,击败了100.00% 的Java用户
     // 内存消耗:38.4 MB,击败了57.65% 的Java用户
-    public static void rotate5(int[] nums, int k) {
+    public static void rotate4(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
+    public static void reverse(int nums[], int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
+        int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int k = 3;
-        rotate3(nums, k);
+        rotate4(nums, k);
+        // reverse(nums, 5, 9);
         System.out.println(Arrays.toString(nums));
         // System.out.println((6-0)%7);
+
     }
 
 }
