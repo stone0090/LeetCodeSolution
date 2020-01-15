0d94d4ab1776eeec0d061d86580ac83395642bdc
stone <jiajie.sjj@alibabapictures.com>
2020-01-15 10:50:10 +0800

    commit 189

--- src/_189_Solution.java
+++ src/_189_Solution.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * @author stone
@@ -8,44 +7,51 @@ import java.util.HashMap;
 public class _189_Solution {
 
     // 解法一，嵌套循环
-    // 执行耗时:106 ms,击败了22.63% 的Java用户
-    // 内存消耗:37.8 MB,击败了90.37% 的Java用户
+    // 执行耗时:104 ms,击败了27.61% 的Java用户
+    // 内存消耗:37.3 MB,击败了95.41% 的Java用户
     public static void rotate1(int[] nums, int k) {
-
-    }
-
-    // 解法二，使用额外HashMap，一次循环
-    // 执行耗时:8 ms,击败了38.72% 的Java用户
-    // 内存消耗:38.7 MB,击败了41.36% 的Java用户
-    public static void rotate2(int[] nums, int k) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int index = (i + k) % nums.length;
-            if (!hm.containsKey(index)) {
-                hm.put(index, nums[index]);
-            }
-            if (hm.containsKey(i)) {
-                nums[index] = hm.get(i);
-            } else {
-                hm.put(i, nums[i]);
-                nums[index] = nums[i];
-
+        for (int i = 0; i < k; i++) {
+            int previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                int temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
             }
         }
     }
 
-    // 解法三，使用额外数组，两次循环
+    // 解法二，使用额外数组，两次循环
     // 执行耗时:1 ms,击败了81.21% 的Java用户
     // 内存消耗:37.4 MB,击败了95.41% 的Java用户
-    public static void rotate3(int[] nums, int k) {
-
+    public static void rotate2(int[] nums, int k) {
+        int[] temps = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int index = (k + i) % nums.length;
+            temps[index] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = temps[i];
+        }
     }
 
     // 解法四
     // 执行耗时:1 ms,击败了81.21% 的Java用户
     // 内存消耗:36.6 MB,击败了95.45% 的Java用户
-    public static void rotate4(int[] nums, int k) {
-
+    public static void rotate3(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int i = 0; count < nums.length; i++) {
+            int current_index = i;
+            int previous = nums[i];
+            do {
+                int next_index = (k + count) % nums.length;
+                int temp = nums[next_index];
+                nums[next_index] = previous;
+                previous = temp;
+                current_index = next_index;
+                count++;
+            } while (i != current_index);
+        }
     }
 
     // 解法五
@@ -58,7 +64,7 @@ public class _189_Solution {
     public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7};
         int k = 3;
-        rotate2(nums, k);
+        rotate3(nums, k);
         System.out.println(Arrays.toString(nums));
         // System.out.println((6-0)%7);
     }
